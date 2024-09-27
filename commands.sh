b4aec4230f91d7a5ed723b246e39cd7b91d2fb7d
Sriram Suresh <sriram.suresh449@gmail.com>
2024-09-27 09:13:49 -0500

    clean up

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@ apt-get install build-essential -y
 apt-get install librabbitmq-dev -y
 
 
-rm send receive
+# rm send receive
 g++ -o send send.cpp -lrabbitmq
 g++ -o receive receive.cpp -lrabbitmq
 # ./send
