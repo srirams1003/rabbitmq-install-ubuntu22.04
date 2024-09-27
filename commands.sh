b31f6202df347186a5bad1507e6b7f69888857f6
Sriram Suresh <sriram.suresh449@gmail.com>
2024-09-27 08:51:00 -0500

    got rabbitmq and debian containers to work together using docker compose

---
+++ commands.sh
@@ -0,0 +1,13 @@
+cd /usr/src/app
+
+apt-get update
+apt-get install build-essential -y
+apt-get install librabbitmq-dev -y
+
+
+rm send receive
+g++ -o send send.cpp -lrabbitmq
+g++ -o receive receive.cpp -lrabbitmq
+# ./send
+# ./receive
+
--- run_docker_rabbitmq.sh
+++ run_docker_rabbitmq.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-# Run RabbitMQ container
-docker run -d --rm --name rabbitmq -p 5552:5552 -p 15672:15672 -p 5672:5672 \
-    -e RABBITMQ_SERVER_ADDITIONAL_ERL_ARGS='-rabbitmq_stream advertised_host localhost' \
-    rabbitmq:3.13
+# Bring up the Docker containers
+docker-compose up -d
 
 # Wait for RabbitMQ container to be ready
 echo "Waiting for RabbitMQ to start..."
 sleep 20
 
-# Enable rabbitmq_stream and rabbitmq_stream_management plugins
-docker exec rabbitmq rabbitmq-plugins enable rabbitmq_stream rabbitmq_stream_management
+# Enable rabbitmq_stream and rabbitmq_stream_management plugins for RabbitMQ
+docker exec rabbitmq_container rabbitmq-plugins enable rabbitmq_stream rabbitmq_stream_management
 
+# Copy the commands.sh script into the Debian container
+docker cp commands.sh debian_container:/usr/src/app/commands.sh
+
+# Make the script executable
+docker exec debian_container chmod +x /usr/src/app/commands.sh
+
+# Execute the commands.sh script in the Debian container
+docker exec debian_container bash /usr/src/app/commands.sh
+
+# Access the Debian container (if you want to continue working interactively)
+docker exec -it debian_container /bin/bash
+
+docker exec debian_container bash -c "cd /usr/src/app"
